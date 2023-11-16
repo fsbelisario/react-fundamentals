2f519e6e1b150402d90b55088f04ac0126e62c65
Belisario Felipe <fbelisario@gmail.com>
2023-11-16 20:44:01 -0300

    Testes com useEffect, useLayoutEffect e eventListeners
    Testes com useEffect, useLayoutEffect e eventListeners

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
 
 import { ThemeProvider } from 'styled-components';
 
@@ -12,14 +12,29 @@ function App() {
   const [theme, setTheme] = useState('dark');
 
   const currentTheme = useMemo(() => {
-    console.log(themes[theme] || themes.dark)
-    console.log((themes[theme] || themes.dark).spacing.small)
     return themes[theme] || themes.dark;
   }, [theme]);
 
   function handleToggleTheme() {
     setTheme((prevState) => prevState === 'dark' ? 'light' : 'dark');
   }
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+
+    return () => {
+      console.debug('Component unmounted');
+    }
+  }, [theme]);
+
+  useLayoutEffect(() => {
+    //Execução síncrona antes do carregamento da tela
+    console.debug('useLayoutEffect executed');
+
+    return () => {
+      console.debug('Component unmounted');
+    }
+  }, [theme]);
 
   return (
     <ThemeProvider theme={currentTheme}>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Header from '../Header';
 import PostsList from '../PostsList';
 import Footer from '../Footer';
 
 export default function Layout({ selectedTheme, onToggleTheme }) {
+
+  useEffect(() => {
+    function handleScroll() {
+      console.debug('scroll executed');
+    }
+
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+      console.debug('Component unmounted & eventListener removed');
+    }
+  }, [])
+
   return (
     <>
       <Header
